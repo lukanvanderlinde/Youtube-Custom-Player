f617c7f56da058f308da195d80929f4bc003a285
Lukan Vanderlinde <lukanvanderlinde@gmail.com>
2022-04-26 20:59:28 -0300

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,5 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  theme: {
-    colors: {
-      black: '#000000',
-      white: '#ffffff',
-      'vtex-pink-4': '#DA3561',
-      'vtex-pink-5': '#FBF2F5',
-    },
-  },
+  theme: {},
   plugins: [],
 }
