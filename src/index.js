384963bd2b7a0de566c4e261d1a9edb98cf53112
Lukan Vanderlinde <lukanvanderlinde@gmail.com>
2022-04-26 20:55:42 -0300

    safety

--- src/index.js
+++ src/index.js
@@ -12,12 +12,12 @@ import Dashboard from 'routes/dashboard'
 const container = document.getElementById('root')
 const root = createRoot(container)
 const firebaseConfig = {
-  apiKey: 'AIzaSyCCvSuN1_m3qTvYl3NqnBT0G_oCr3_8xqA',
-  authDomain: 'mydashboard-c6c38.firebaseapp.com',
-  projectId: 'mydashboard-c6c38',
-  storageBucket: 'mydashboard-c6c38.appspot.com',
-  messagingSenderId: '312250233056',
-  appId: '1:312250233056:web:4470abd45b9f4280816085',
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 }
 const app = initializeApp(firebaseConfig)
 
